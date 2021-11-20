5c21af7386af2ed6dcf81581b3fa0eae2e94bcfb
Bilal Karakan <85422865+BilalKarakan@users.noreply.github.com>
2021-11-20 07:36:07 +0300

    Add files via upload

---
+++ src/models/user.js
@@ -0,0 +1,9 @@
+export default class User {
+    constructor(id, firstName, newFirstName, city) {
+        // console.log("user oluştu.")
+        this.id = id
+        this.firstName = firstName
+        this.newFirstName = newFirstName
+        this.city = city
+    }
+}
